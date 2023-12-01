0df2d6b45e78e26def4c507d2659b3a7fa7c6a7b
seon <sunkyoung.dev@gmail.com>
2023-12-01 10:48:21 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import plotly.graph_objects as go
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+def main():
+    st.title("주식 데이터")
+    st.sidebar.title("Stock Chart")
+    ticker = st.sidebar.text_input("Enter a ticker (e. g. AAPL)", value = "AAPL")
+    st.sidebar.markdown('Tickers Link : [All Stock Symbols](https://stockanalysis.com/stocks/)')
+    start_date = st.sidebar.date_input("시작 날짜: ", value = pd.to_datetime("2023-01-01"))
+    end_date = st.sidebar.date_input("종료 날짜: ", value = pd.to_datetime("2023-07-28"))
+
+	#ticker 종목의 시작~종료 날짜 사이의 가격변화를 데이터로 보여줌
+    data = yf.download(ticker, start= start_date, end= end_date)
+    st.dataframe(data)
+
+     #Line Chart, Candle Stick 선택형으로 만들기
+    chart_type = st.sidebar.radio("Select Chart Type", ("Candle_Stick", "Line"))
+    candlestick = go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'])
+    line = go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Close')
+
+    if chart_type == "Candle_Stick":
+        fig = go.Figure(candlestick)
+    elif chart_type == "Line":
+        fig = go.Figure(line)
+    else:
+        st.error("error")
+
+    fig.update_layout(title=f"{ticker} Stock {chart_type} Chart", xaxis_title="Date", yaxis_title="Price")
+    st.plotly_chart(fig)
+    
+    
+if __name__ == "__main__":
+    main()
