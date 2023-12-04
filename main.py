9f1176a748c0f8d52edbfaf831e548a29aa2be90
seon <sunkyoung.dev@gmail.com>
2023-12-04 13:09:26 +0900

    t3

--- main.py
+++ main.py
@@ -62,7 +62,8 @@ def to_excel(df):
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
     format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1) 
+    worksheet.set_column('A:A', None, format1)
+    writer.close()
     processed_data = output.getvalue()
     return processed_data
 df_xlsx = to_excel(filter_data)
